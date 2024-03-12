30e3e82e83f5081b23f42554c7e08f8ad6e65685
wirabudhi <bpantaiselatan@gmail.com>
2024-03-13 02:21:28 +0800

    Update .env.example and guest.blade.php files

---
+++ resources/views/livewire/footer.blade.php
@@ -0,0 +1,13 @@
+<div>
+    <footer class="footer footer-center p-10 bg-gradient-to-t from-orange-700 to-orange-600 text-base-content rounded">
+        <nav class="grid grid-flow-col gap-4 text-white">
+            <a class="link link-hover" href="{{ url('/') }}">Beranda</a>
+            <a class="link link-hover" href="{{ url('/portfolios') }}">Portfolio</a>
+            <a class="link link-hover" href="{{ url('/produk') }}">Produk</a>
+            <a class="link link-hover" href="{{ url('/faqs') }}">FAQ</a>
+        </nav>
+        <aside class="text-white">
+            <p>Copyright © 2024 - All right reserved by Pw Interior</p>
+        </aside>
+    </footer>
+</div>
